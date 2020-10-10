748e33758b7fb8d6cd2eb451f73cd946155b5797
Orsolya <orsolya.suto@ericsson.com>
2020-10-10 12:29:25 +0200

    introducing two new functions

--- script.js
+++ script.js
@@ -3,35 +3,70 @@ let playing = false;
 let score;
 let gameOver = document.getElementById("game-over");
 let satrtResetBtn = document.getElementById("start-reset-btn");
+let questions = [
+    {
+        id: 1,
+        question: '2 x 2',
+        answerRight: 4,
+        answerWrongA: 5,
+        answerWrongB: 6,
+        answerWrongC: 94,
+    },
+    {
+        id: 2,
+        question: '4 x 5',
+        answerRight: 9,
+        answerWrongA: 34,
+        answerWrongB: 22,
+        answerWrongC: 43,
+    },
+    {
+        id: 3,
+        question: '12 - 9',
+        answerRight: 3,
+        answerWrongA: 4,
+        answerWrongB: 6,
+        answerWrongC: 97,
+    },
+
+];
 //if we click on the start/reset button
 function startResetGame() {
-  console.log("helllo");
-  playing = true;
-  //if we are playing
-  if (!playing) {
-    location.reload(); //reload the page
-  } else {
+    //if we are playing
+    if (playing) {
+        location.reload(); //reload the page
+    } else {
+    playing = true;
     //if we are not playing
-    console.log("else -----");
     score = 0; //set score to 0
     document.getElementById("score-value").innerHTML = score;
     start = 6;
     //show countdown box
-    document.getElementById("time-remaining").style.display = "block";
+    showElement("time-remaining");
     let timeRemainingValue = document.getElementById("time-remaining-value");
+    satrtResetBtn.innerHTML = "Reset Game"; //change button to reset
     let countingDown = setInterval(function () {
       start--; //reduce time by 1sec in loops
       timeRemainingValue.innerHTML = start;
       if (start == 0) {
         //no -> game over
         clearInterval(countingDown);
-        gameOver.style.display = "block";
+        showElement("game-over");
         gameOver.innerHTML = `Game Over <br/> Your score is ${score}`;
-        satrtResetBtn.innerHTML = "Reset Game"; //change button to reset
+        hideElement("time-remaining");
       }
     }, 1000);
   }
 }
+
+function hideElement (id) {
+    document.getElementById(id).style.display = 'none';
+}
+
+function showElement (id) {
+    document.getElementById(id).style.display = 'block';
+}
+
 
 //timeleft?
 //yes -> continue
