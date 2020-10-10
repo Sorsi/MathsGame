ee9778a56743e54ae93b726f1706b0a6fe287b95
Orsolya <orsolya.suto@ericsson.com>
2020-10-10 12:01:30 +0200

    expanding logiic

--- script.js
+++ script.js
@@ -1,39 +1,49 @@
+/*jshint esversion: 6 */
+let playing = false;
+let score;
+let gameOver = document.getElementById("game-over");
+let satrtResetBtn = document.getElementById("start-reset-btn");
 //if we click on the start/reset button
-var playing = false;
-var score;
 function startResetGame() {
-    playing = true;
-    //if we are playing
-    if (playing) {
-        location.reload(); //reload the page
-    } else { //if we are not playing
-        score = 0; //set score to 0
-        document.getElementById('score-value').innerHTML = score;
-        start = 60;
-        document.getElementById('time-remaining').style.display = 'block';
-        var timeRemainingValue = document.getElementById('time-remaining-value');
-        var countingDown = setInterval(function() {
-            start--; timeRemainingValue.innerHTML = start;
-        }, 1000);
-        countingDown();
-
-    }
+  console.log("helllo");
+  playing = true;
+  //if we are playing
+  if (!playing) {
+    location.reload(); //reload the page
+  } else {
+    //if we are not playing
+    console.log("else -----");
+    score = 0; //set score to 0
+    document.getElementById("score-value").innerHTML = score;
+    start = 6;
+    //show countdown box
+    document.getElementById("time-remaining").style.display = "block";
+    let timeRemainingValue = document.getElementById("time-remaining-value");
+    let countingDown = setInterval(function () {
+      start--; //reduce time by 1sec in loops
+      timeRemainingValue.innerHTML = start;
+      if (start == 0) {
+        //no -> game over
+        clearInterval(countingDown);
+        gameOver.style.display = "block";
+        gameOver.innerHTML = `Game Over <br/> Your score is ${score}`;
+        satrtResetBtn.innerHTML = "Reset Game"; //change button to reset
+      }
+    }, 1000);
+  }
 }
 
-        //show countdown box
-        //reduce time by 1sec in loops
-            //timeleft?
-                //yes -> continue
-                //no -> game over
-            //change button to reset
-            //generate a new question
+//timeleft?
+//yes -> continue
+
+//generate a new question
 
 //if we click on the answer box
-    //if we are playing
-        //correct answer?
-            //yes
-                //incrase score
-                //show correct box for 1 sec
-                //generate new question
-            //no
-                //show try again box for 1 sec
+//if we are playing
+//correct answer?
+//yes
+//incrase score
+//show correct box for 1 sec
+//generate new question
+//no
+//show try again box for 1 sec
