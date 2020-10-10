1ddf974ea159653e27c6f585b6f0a081d05e1123
Orsolya <orsolya.suto@ericsson.com>
2020-10-10 12:54:57 +0200

    small corrections

--- script.js
+++ script.js
@@ -3,70 +3,82 @@ let playing = false;
 let score;
 let gameOver = document.getElementById("game-over");
 let satrtResetBtn = document.getElementById("start-reset-btn");
+let timeRemainingValue = document.getElementById("time-remaining-value");
 let questions = [
-    {
-        id: 1,
-        question: '2 x 2',
-        answerRight: 4,
-        answerWrongA: 5,
-        answerWrongB: 6,
-        answerWrongC: 94,
-    },
-    {
-        id: 2,
-        question: '4 x 5',
-        answerRight: 9,
-        answerWrongA: 34,
-        answerWrongB: 22,
-        answerWrongC: 43,
-    },
-    {
-        id: 3,
-        question: '12 - 9',
-        answerRight: 3,
-        answerWrongA: 4,
-        answerWrongB: 6,
-        answerWrongC: 97,
-    },
-
+  {
+    id: 1,
+    question: "2 x 2",
+    answerRight: 4,
+    answerWrongA: 5,
+    answerWrongB: 6,
+    answerWrongC: 94,
+  },
+  {
+    id: 2,
+    question: "4 x 5",
+    answerRight: 9,
+    answerWrongA: 34,
+    answerWrongB: 22,
+    answerWrongC: 43,
+  },
+  {
+    id: 3,
+    question: "12 - 9",
+    answerRight: 3,
+    answerWrongA: 4,
+    answerWrongB: 6,
+    answerWrongC: 97,
+  },
 ];
 //if we click on the start/reset button
 function startResetGame() {
-    //if we are playing
-    if (playing) {
-        location.reload(); //reload the page
-    } else {
+  if (playing) { //if we are playing
+    location.reload(); //reload the page
+  } else { //if we are not playing
+    hideElement("game-over");
     playing = true;
-    //if we are not playing
-    score = 0; //set score to 0
+    score = 66; //set score to 0
     document.getElementById("score-value").innerHTML = score;
     start = 6;
-    //show countdown box
-    showElement("time-remaining");
-    let timeRemainingValue = document.getElementById("time-remaining-value");
+    showElement("time-remaining"); //show countdown box
     satrtResetBtn.innerHTML = "Reset Game"; //change button to reset
-    let countingDown = setInterval(function () {
-      start--; //reduce time by 1sec in loops
-      timeRemainingValue.innerHTML = start;
-      if (start == 0) {
-        //no -> game over
-        clearInterval(countingDown);
-        showElement("game-over");
-        gameOver.innerHTML = `Game Over <br/> Your score is ${score}`;
-        hideElement("time-remaining");
-      }
-    }, 1000);
+    startCountDown();
+    generateQuestionAndAnswers();
   }
 }
 
-function hideElement (id) {
-    document.getElementById(id).style.display = 'none';
+function startCountDown() {
+  countingDown = setInterval(function () {
+    start--; //reduce time by 1sec in loops
+    timeRemainingValue.innerHTML = start;
+    if (start == 0) {
+      stopCountDown();
+      showElement("game-over");
+      gameOver.innerHTML = `Game Over <br/> Your score is ${score}`; //no -> game over
+      hideElement("time-remaining");
+      hideElement("correct");
+      hideElement("wrong");
+      playing = false;
+      satrtResetBtn.innerHTML = "Start Game";
+    }
+  }, 1000);
 }
 
-function showElement (id) {
-    document.getElementById(id).style.display = 'block';
+function stopCountDown() {
+  clearInterval(countingDown);
 }
 
+function generateQuestionAndAnswers() {
+
+}
+
+function hideElement(id) {
+  document.getElementById(id).style.display = "none";
+}
+
+function showElement(id) {
+  document.getElementById(id).style.display = "block";
+}
 
 //timeleft?
 //yes -> continue
